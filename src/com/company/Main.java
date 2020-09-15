e988f1ad4bd8abb8b12a020250376b1ac55243f6
”Ivan <tishchienko_2001@mail.ru>
2020-09-15 11:08:36 +0300

    Commit 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList LinkedList = new LinkedList();
+        LinkedList.add(10);
+        LinkedList.add(20);
+        LinkedList.add(1);
+        LinkedList.minValue();
+        LinkedList.print();
+    }
+}
+
